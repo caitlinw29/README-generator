e1f048893d9d1cd8e8c7987dd1cfc8485551a661
Caitlin Wall <caitlinwall96@gmail.com>
2021-10-07 11:44:39 -0400

    Create package.json and add inquirer

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', process.argv[2], (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
