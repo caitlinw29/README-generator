c8be52c9015e47ecbc49d4253f3c2d645253ea05
Caitlin Wall <caitlinwall96@gmail.com>
2021-10-07 16:12:41 -0400

    Add inquirer questions

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown.js');
+const path = require('path');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What are the licenses?',
+        choices: ['Apache', 'Boost', 'MIT', 'IBM'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile('README.md', process.argv[2], (err) =>
-  err ? console.error(err) : console.log('Success!')
-);
-}
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+ }
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile();
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // const readmePageContent = generateREADME(answers);
+
+            // fs.writeFile('README.md', readmePageContent, (err) =>
+            //     err ? console.log(err) : console.log('Successfully created README!')
+            // );
+            console.log(answers);
+        });
 }
 
 // Function call to initialize app
 init();
+
+
+
+
+// const generateREADME = (answers) =>
+// ``;
+
+
